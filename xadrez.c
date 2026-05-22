70012b103eccde8ec777ededb4f1d352ea5e8ef8
Sehtz <sssergiosilva745@gmail.com>
2026-05-22 23:46:56 +0000

    Desafio Movimentação Xadrez: Nível Novato.

--- xadrez.c
+++ xadrez.c
@@ -4,18 +4,70 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
+int main()
+{
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    printf("\n     ***XADREZ***\n\n");
+
+    printf("*Movimentação da peça: Bispo*\n\n");
+
+    printf("Ponto de partida\n");
+
+    // Declaração de variáveis e logica.
+
+    // Movimentação de 5 casas na diagonal.
+
+    int bispo = 0;
+    while (bispo < 5)
+    {
+        printf("%dº Movimento: Cima, direita.\n", bispo + 1);
+        bispo++;
+    }
+    printf("Ponto de chegada\n\n");
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
 
+    printf("\n     ***XADREZ***\n\n");
+
+    printf("*Movimentação da peça: Torre*\n\n");
+
+    printf("Ponto de partida\n");
+
+    // Declaração de variáveis e logica.
+
+    // Movimentação de 5 casas para a direita.
+    for (int torre = 0; torre < 5; torre++)
+    {
+        printf("%dº Movimento: Direita.\n", torre + 1);
+    }
+    printf("Ponto de chegada\n\n");
+
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    printf("\n     ***XADREZ***\n\n");
+
+    printf("*Movimentação da peça: Rainha*\n\n");
+
+    printf("Ponto de partida\n");
+
+    // Declaração de variáveis e logica.
+
+    // Movimentação de 5 casas na diagonal.
+
+    int rainha = 0;
+
+    do
+    {
+        printf("%dº Movimento: Esquerda.\n", rainha + 1);
+        rainha++;
+    } while (rainha < 8);
+
+    printf("Ponto de chegada\n\n");
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
